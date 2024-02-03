f01eb929a51a840a6d30d903b4ac4efb85fd22ae
chaimae <shaimaabouassab@gmail.com>
2024-02-04 00:42:20 +0100

    moved business logic to Auth.controller.js and route configuration to Auth.route.js.

--- Controllers/Auth.Controller.js
+++ Controllers/Auth.Controller.js
@@ -5,130 +5,146 @@ const createError = require("http-errors");
 const User = require("../Models/User.models");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const { signAccessToken,signRefreshToken } = require("../helpers/jwt_helper");
+const { signAccessToken, signRefreshToken } = require("../helpers/jwt_helper");
 
-router.post("/registre", async (req, res) => {
-  try {
-    const { name, email, password, level, github, linkedin, team, year } =
-      req.body;
+module.exports = {
+  register: async (req, res) => {
+    try {
+      const { name, email, password, level, github, linkedin, team, year } =
+        req.body;
 
-    const user = new User({
-      name: name,
-      email: email,
-      password: password,
-      level: level,
-      github: github,
-      linkedin: linkedin,
-      team: team,
-      year: year,
-    });
-    //?check if the user is valid
-    const error = await user.validate();
-    if (error) throw error;
+      const user = new User({
+        name: name,
+        email: email,
+        password: password,
+        level: level,
+        github: github,
+        linkedin: linkedin,
+        team: team,
+        year: year,
+      });
 
-    //?checking if the users's email il aready used
-    const isEmailUsed = await User.isEmailUsed(user.email);
-    if (isEmailUsed)
-      throw new Error(JSON.stringify({ message: "Email is already used" }));
-    //?hasing the password
-    const salt = await bcrypt.genSalt();
-    user.password = await bcrypt.hash(user.password, salt);
-    //?inserting the newuser to the databse
-    user.save();
+      // Validation de l'utilisateur
+      const error = await user.validate();
+      if (error) throw error;
 
-    const token = jwt.sign(
-      {
-        id: user._id,
-        exp: Date.now() + +process.env.TOKEN_EXIPE_TIME,
-        iat: Date.now(),
-      },
-      process.env.ACCESS_TOKEN_SECRET
-    );
+      // Vérification de l'email déjà utilisé
+      const isEmailUsed = await User.isEmailUsed(user.email);
+      if (isEmailUsed)
+        throw new Error(JSON.stringify({ message: "Email is already used" }));
 
-    res.header("auth-token", token).json({ status: "ok" });
-  } catch (e) {
-    res.status(400).send(e.message);
-  }
-});
-router.post("/login", async (req, res) => {
-  try {
-    if (req.body.email === undefined || req.body.password === undefined)
-      throw new Error(
-        JSON.stringify({ message: "email or password is missing" })
-      );
-    const user = await User.findOne({ email: req.body.email });
-    if (user === null)
-      throw new Error(JSON.stringify({ message: "user not found" }));
+      // Hachage du mot de passe
+      const salt = await bcrypt.genSalt();
+      user.password = await bcrypt.hash(user.password, salt);
 
-    if (user.email !== req.body.email.toLowerCase())
-      throw new Error(
-        JSON.stringify({ message: "email or password are incorrect" })
+      // Enregistrement de l'utilisateur
+      await user.save();
+
+      // Génération du token d'authentification
+      const token = jwt.sign(
+        {
+          id: user._id,
+          exp: Date.now() + +process.env.TOKEN_EXPIRE_TIME,
+          iat: Date.now(),
+        },
+        process.env.ACCESS_TOKEN_SECRET
       );
 
-    const isPasswordCorrect = await bcrypt.compare(
-      req.body.password,
-      user.password
-    );
-    if (!isPasswordCorrect)
-      throw new Error(
-        JSON.stringify({ message: "email or password  are incorrect" })
+      // Envoi du token dans l'en-tête de la réponse
+      res.header("auth-token", token).json({ status: "ok" });
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
+  },
+
+  login: async (req, res) => {
+    try {
+      const { email, password } = req.body;
+
+      if (!email || !password)
+        throw new Error(
+          JSON.stringify({ message: "email or password is missing" })
+        );
+
+      const user = await User.findOne({ email: email });
+
+      if (!user) throw new Error(JSON.stringify({ message: "user not found" }));
+
+      if (user.email.toLowerCase() !== email.toLowerCase())
+        throw new Error(
+          JSON.stringify({ message: "email or password are incorrect" })
+        );
+
+      const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+      if (!isPasswordCorrect)
+        throw new Error(
+          JSON.stringify({ message: "email or password are incorrect" })
+        );
+
+      // Génération du token d'authentification
+      const token = jwt.sign(
+        {
+          id: user._id,
+          exp: Date.now() + +process.env.TOKEN_EXPIRE_TIME,
+          iat: Date.now(),
+        },
+        process.env.ACCESS_TOKEN_SECRET
       );
 
-    const token = jwt.sign(
-      {
-        id: user._id,
-        exp: Date.now() + +process.env.TOKEN_EXIPE_TIME,
-        iat: Date.now(),
-      },
-      process.env.ACCESS_TOKEN_SECRET
-    );
-    res.header("auth-token", token).json({ status: "ok" });
-  } catch (e) {
-    res.status(400).send(e.message);
+      // Envoi du token dans l'en-tête de la réponse
+      res.header("auth-token", token).json({ status: "ok" });
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
+  },
+
+
+  changePassword: async (req, res) => {
+    try {
+      const { email, oldPassword, newPassword } = req.body;
+
+      if (!email)
+        throw new Error("email is missing");
+
+      if (!oldPassword)
+        throw new Error("old password is missing");
+
+      if (!newPassword)
+        throw new Error("new password is missing");
+
+      const user = await User.findOne({ email: email });
+
+      if (!user)
+        throw new Error(JSON.stringify({ message: "user not found" }));
+
+      const isPasswordCorrect = await bcrypt.compare(oldPassword, user.password);
+
+      if (!isPasswordCorrect)
+        throw new Error(JSON.stringify({ message: "old password is incorrect" }));
+
+      const salt = await bcrypt.genSalt();
+      const hashedPassword = await bcrypt.hash(newPassword, salt);
+
+      user.password = hashedPassword;
+      await user.save();
+
+      res.status(200).send({ status: "ok" });
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
+  },
+
+  refreshToken: async (req, res) => {
+    const { email, password } = req.body;
+    // Logique de rafraîchissement du token
+    res.send("refresh token route");
+  },
+
+  logout: async (req, res) => {
+    // Logique de déconnexion
+    res.send("logout route");
   }
-});
 
-router.post("/changepassword", async (req, res) => {
-  try {
-    const { email } = req.body;
-    if (email === undefined) throw new Error("email is missing");
-    const { oldPassword } = req.body;
-    if (oldPassword === undefined) throw new Error("old password is missing");
-    //?check if the old password is correct
-    const { newPassword } = req.body;
-    if (newPassword === undefined) throw new Error("new password is missing");
-
-    const user = await User.findOne({ email: email });
-    if (!user) throw new Error(JSON.stringify({ message: "user not found" }));
-    const isPasswordCorrect = await bcrypt.compare(oldPassword, user.password);
-
-    if (!isPasswordCorrect)
-      throw new Error(JSON.stringify({ message: "old password is incorrect" }));
-
-    const salt = await bcrypt.genSalt();
-    const hashedPassword = await bcrypt.hash(newPassword, salt);
-
-    user.password = hashedPassword;
-    await user.save();
-    res.status(200).send({ status: "ok" });
-  } catch (e) {
-    res.status(400).send(e.message);
-  }
-});
-
-router.post("/refresh-token", async (req, res, next) => {
-  const { email, password } = req.body;
-
-  res.send("refresh token route");
-});
-
-router.post("/register", async (req, res, next) => {
-  res.send("register route");
-  res.status(200).json({ message: "Registration successful", user: { email } });
-});
-
-router.delete("/logout", async (req, res, next) => {
-  res.send("logout route");
-});
-
-module.exports = router;
+  
+};
